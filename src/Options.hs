e639583d95633a0f090823a9bb41a1da6249440e
Ketil Malde <ketil@malde.org>
2010-10-15 11:40:11 +0200

    Forgot to add Options.hs - whoops
    Ignore-this: 6e7be654584a51823375d39459c63406
    
    darcs-hash:d05e4b4c980b19a5081d0ad9377fc60c30b96243

---
+++ src/Options.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
+module Options where
+
+import System.Console.CmdArgs
+import Control.Monad (when)
+
+version :: String
+version = "area50 v0 - graphical comparison of genome assemblies, ©2010 Ketil Malde."
+
+data Opt = Opt { outfile  :: FilePath
+               , terminal :: String
+               , expect   :: [String]
+               , inputs   :: [FilePath]
+               , estref   :: FilePath
+               } deriving (Typeable, Data, Show, Eq)
+
+modes :: Mode Opt
+modes = mode $ Opt 
+  { outfile = def &= text "Output file, if applicable" & typFile
+  , terminal= def &= text "Gnuplot output format ('terminal')"
+  , expect  = []  &= text "Expected genome size"
+  , inputs  = def &= args & typFile
+  , estref  = def &= text "Reference transcripts" & typFile
+  }
+
+getArgs :: IO Opt
+getArgs = do
+  o <- cmdArgs version [modes]
+  when (null $ inputs o) $ error "Please specify one or more input files!"
+  -- todo: check that output is specified with terminal!
+  return o
